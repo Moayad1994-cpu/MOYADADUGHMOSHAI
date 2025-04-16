e2c8f62d15e3435b15af77a2e311c38790590098
Moayad1994-cpu <173873078+Moayad1994-cpu@users.noreply.github.com>
2025-04-16 12:32:28 +0400

    app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import waitress
 app = Flask(__name__)
 
 # API Key for Google Generative AI
-API_KEY = 'AIzaSyBTHPyblMw0qtXYTDpgMmMOhvZuh2MtsRo'
+API_KEY = 'AIzaSyAmooXBesmJl_6rks5_mVyhru6XbDwBzbQ'
 
 # Configure the AI API
 try:
